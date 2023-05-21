a190371fc3464dc9e2f52c8b3b22dba55a57a483
Khalimov Javokhir <104940374+JavokhirKhalimov@users.noreply.github.com>
2023-05-21 19:11:57 +0500

    new changes1

--- Form2.cs
+++ Form2.cs
@@ -16,39 +16,43 @@ namespace Testing
     {
         private int remainingSeconds;
         string[] savollar;
-        List<string[]> javoblar=new List<string[]>();
-        Panel panel = new Panel();
-        
+        List<string[]> variantlar = new List<string[]>();
+        string[] path= { @"C:\Users\user\source\repos\Testing\bin\Debug\savollar.txt",
+            @"C:\Users\user\source\repos\Testing\bin\Debug\variantlar.txt",
+            @"C:\Users\user\source\repos\Testing\bin\Debug\javoblar.txt"
 
-        
+        };
+        Panel panel = new Panel();
+
+
+
 
         public Form2()
         {
             InitializeComponent();
             // Устанавливаем начальное значение времени в секундах
-            remainingSeconds =  1800; // 30 минут
+            remainingSeconds = 1800; // 30 минут
             // Устанавливаем интервал таймера в 1 секунду
             timer.Interval = 1000;
             // Запускаем таймер
-            
+
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             //сборка ссылки
-            string[] path = { @"C:\Users\user\source\repos\Testing\bin\Debug\savollar.txt",
-            @"C:\Users\user\source\repos\Testing\bin\Debug\javoblar.txt"};
+            
             //чтение ссылки
             string readAllFile_savollar = File.ReadAllText(path[0]);
             string readAllFile_javoblar = File.ReadAllText(path[1]);
             savollar = readAllFile_savollar.Split('\n');
             string[] lines = readAllFile_javoblar.Split('\n');
 
-            
+
             foreach (string line in lines)
             {
                 string[] items = line.Split(',');
-                javoblar.Add(items);
+                variantlar.Add(items);
             }
 
 
@@ -79,9 +83,9 @@ namespace Testing
         {
             button1.Enabled = false;
             timer.Start();
-            int x = 10,  a=555, b=0;
+            int x = 10, a = 555, b = 0;
 
-           // Panel panel = new Panel();
+            // Panel panel = new Panel();
             panel.Location = new Point(10, 10);
             panel.Size = new Size(800, 600);
             this.Controls.Add(panel);
@@ -94,66 +98,75 @@ namespace Testing
             Random random = new Random();
             int radioButtonOffset = 20;
 
-            for (int j=0; j<savollar.Length; j++)
+            for (int j = 0; j < savollar.Length; j++)
             {
                 GroupBox questionGroup = new GroupBox();
-                questionGroup.Location = new Point(3, 5+b);
+                questionGroup.Location = new Point(3, 5 + b);
                 questionGroup.Size = new Size(555, 155);
                 questionGroup.Text = savollar[j];
                 panel.Controls.Add(questionGroup);
                 b += 155;
-                
+
                 // Создание радиокнопок с вариантами ответа
                 for (int i = 0; i < 3; i++)
                 {
-                    List<string> list = new List<string>(javoblar[j]);
+                    List<string> list = new List<string>(variantlar[j]);
                     int randomNumber = random.Next(0, list.Count);
 
                     RadioButton radioButton = new RadioButton();
-                    radioButton.Location = new Point(x , 30 + radioButtonOffset * i);
-                    radioButton.Text = list[randomNumber];
+                    radioButton.Location = new Point(x, 30 + radioButtonOffset * i);
+                    radioButton.Text = '\u200B' + list[randomNumber];
                     questionGroup.Controls.Add(radioButton);
 
                     list.RemoveAt(randomNumber);
-                    javoblar[j] = list.ToArray();
+                    variantlar[j] = list.ToArray();
 
                 }
-                
 
-                
+
+
             }
 
-            
 
-            
+
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            
+            
+            string[] lines = File.ReadAllLines(path[2]);
+
+
             try
             {
                 timer.Stop();
 
                 // Переменная для подсчета правильных ответов
                 int correctAnswers = 0;
-
+                int count = 0;
                 // Перебираем все GroupBox на панели
                 foreach (GroupBox questionGroup in panel.Controls.OfType<GroupBox>())
                 {
+                    
                     // Находим выбранную радиокнопку внутри GroupBox
                     RadioButton selectedRadioButton = questionGroup.Controls.OfType<RadioButton>()
                         .FirstOrDefault(radioButton => radioButton.Checked);
 
                     // Получаем индекс правильного ответа для текущего вопроса
                     int questionIndex = panel.Controls.IndexOf(questionGroup);
-                    int correctAnswerIndex = 0;
 
                     // Проверяем ответ студента
-                    if (selectedRadioButton != null && selectedRadioButton.Tag != null && selectedRadioButton.Tag.ToString() == correctAnswerIndex.ToString())
+                    if (selectedRadioButton != null && selectedRadioButton.Text != null &&
+                    selectedRadioButton.Text.Trim() == '\u200B' + lines[count].Trim())
                     {
                         // Ответ верный
                         correctAnswers++;
                     }
+                    //MessageBox.Show(selectedRadioButton.Text + ":" + lines[count]);
+                    count++;
+                    
                 }
 
                 // Выводим результаты
@@ -163,14 +176,14 @@ namespace Testing
                 this.Close();
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
-            
+
         }
     }
 
-        
-    
+
+
 }
